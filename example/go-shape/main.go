973106f0c5a7d8ebc2e7bd0ecd405ba4cba6a3bc
Nguyễn Mạnh Cường <cuongnm72@viettel.com.vn>
2024-03-03 20:35:24 -0800

    add subscriber

--- example/go-shape/main.go
+++ example/go-shape/main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
+	//************PUBLISHER**********************
 	pa_ := fastdds.NewParticipant("Participant_pub")
 	type_ := fastdds.NewTypeSupport()
 	fastdds.RegisterType(pa_, type_)
@@ -14,6 +15,18 @@ func main() {
 	listener_ := fastdds.NewPubListener()
 	fmt.Println(listener_)
 	writer_ := fastdds.CreateWriter(pub_, topic_, listener_)
-	shape := fastdds.NewShape("red")
-	fastdds.SendMessage(writer_, listener_, shape)
+	for {
+		shape := fastdds.NewShape("red")
+		fastdds.SendMessage(writer_, listener_, shape)
+	}
+	//************SUBSCRIBER**********************
+	// pa_ := fastdds.NewParticipant("Participant_sub")
+	// type_ := fastdds.NewTypeSupport()
+	// fastdds.RegisterType(pa_, type_)
+	// topic_ := fastdds.CreateTopic(pa_, type_, "ShapeTopic")
+	// sub_ := fastdds.NewSubscriber(pa_)
+	// lis_ := fastdds.NewSubscriberListener()
+	// go fastdds.CreateDataReader(sub_, topic_, lis_)
+	// var ch chan int
+	// <-ch // locking
 }
